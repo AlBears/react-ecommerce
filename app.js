871460ad810f9d55bab8dc7fb596287af1446ae6
Alexey Sidorovitch <albears@yandex.ru>
2019-12-11 16:35:24 +0300

    routes

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 require("dotenv").config();
+const userRoutes = require('./routes/user');
 
 // app
 const app = express();
@@ -14,9 +15,7 @@ mongoose
     .then(() => console.log("DB Connected"));
 
 // routes
-app.get("/", (req, res) => {
-    res.send("hello from node");
-});
+app.use("/api", userRoutes);
 
 const port = process.env.PORT || 8000;
 
