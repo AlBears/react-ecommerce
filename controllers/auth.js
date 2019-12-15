f782bf4355521e09844c41a04195d874aceea97c
Alexey Sidorovitch <albears@yandex.ru>
2019-12-15 19:00:01 +0300

    auth admin middleware

--- controllers/auth.js
+++ controllers/auth.js
@@ -4,7 +4,6 @@ const expressJwt = require("express-jwt"); // for authorization check
 const { errorHandler } = require('../helpers/dbErrorHandler');
 
 exports.signup = (req, res) => {
-    console.log("req.body", req.body);
     const user = new User(req.body);
     user.save((err, user) => {
         if (err) {
@@ -55,3 +54,22 @@ exports.requireSignin = expressJwt({
     secret: process.env.JWT_SECRET,
     userProperty: "auth"
 });
+
+exports.isAuth = (req, res, next) => {
+    let user = req.profile && req.auth && req.profile._id == req.auth._id;
+    if (!user) {
+        return res.status(403).json({
+            error: "Access denied"
+        });
+    }
+    next();
+};
+
+exports.isAdmin = (req, res, next) => {
+    if (req.profile.role === 0) {
+        return res.status(403).json({
+            error: "Admin resourse! Access denied"
+        });
+    }
+    next();
+};
